0936d2d367eb839dc3b7d28735a7923df0ba6ae8
= <=>
2022-09-01 00:14:11 -0700

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,124 +1,59 @@
 extern crate proc_macro;
 
-use std::cmp::PartialEq;
-
-use num_enum::{TryFromPrimitive, TryFromPrimitiveError};
 use pyo3::prelude::*;
-use pyo3::types::PyDict;
 use std::include_str;
+use std::collections::HashMap;
 
-/// All the numeric values tokens can have. Taken from here:
-/// https://github.com/python/cpython/blob/main/Lib/token.py
-#[derive(Clone, Debug, Eq, PartialEq, TryFromPrimitive)]
-#[pyclass]
-#[repr(usize)]
-pub enum TokenType {
-    ENDMARKER = 0,
-    NAME = 1,
-    NUMBER = 2,
-    STRING = 3,
-    NEWLINE = 4,
-    INDENT = 5,
-    DEDENT = 6,
-    LPAR = 7,
-    RPAR = 8,
-    LSQB = 9,
-    RSQB = 10,
-    COLON = 11,
-    COMMA = 12,
-    SEMI = 13,
-    PLUS = 14,
-    MINUS = 15,
-    STAR = 16,
-    SLASH = 17,
-    VBAR = 18,
-    AMPER = 19,
-    LESS = 20,
-    GREATER = 21,
-    EQUAL = 22,
-    DOT = 23,
-    PERCENT = 24,
-    LBRACE = 25,
-    RBRACE = 26,
-    EQEQUAL = 27,
-    NOTEQUAL = 28,
-    LESSEQUAL = 29,
-    GREATEREQUAL = 30,
-    TILDE = 31,
-    CIRCUMFLEX = 32,
-    LEFTSHIFT = 33,
-    RIGHTSHIFT = 34,
-    DOUBLESTAR = 35,
-    PLUSEQUAL = 36,
-    MINEQUAL = 37,
-    STAREQUAL = 38,
-    SLASHEQUAL = 39,
-    PERCENTEQUAL = 40,
-    AMPEREQUAL = 41,
-    VBAREQUAL = 42,
-    CIRCUMFLEXEQUAL = 43,
-    LEFTSHIFTEQUAL = 44,
-    RIGHTSHIFTEQUAL = 45,
-    DOUBLESTAREQUAL = 46,
-    DOUBLESLASH = 47,
-    DOUBLESLASHEQUAL = 48,
-    AT = 49,
-    ATEQUAL = 50,
-    RARROW = 51,
-    ELLIPSIS = 52,
-    COLONEQUAL = 53,
-    OP = 54,
-    AWAIT = 55,
-    ASYNC = 56,
-    TYPE_IGNORE = 57,
-    TYPE_COMMENT = 58,
-    SOFT_KEYWORD = 59,
-    // These aren't used by the C tokenizer but are needed for tokenize.py
-    ERRORTOKEN = 60,
-    COMMENT = 61,
-    NL = 62,
-    ENCODING = 63,
-    N_TOKENS = 64,
-    // Special definitions for cooperation with parser
-    NT_OFFSET = 256    
-}
 
 /// The direct Rust equivalent of the Python class of the same name.
 #[derive(Debug, FromPyObject)]
 pub struct TokenInfo {
     #[pyo3(attribute("type"))]
-    token_type: usize,  /// type
-    string: String, /// The token itself
-    start: (usize,usize),  /// Start (line,col)
-    end: (usize,usize),  /// End (line,col)
-    line: String
+    pub token_type: usize,  /// type
+    pub string: String, /// The token itself
+    pub start: (usize,usize),  /// Start (line,col)
+    pub end: (usize,usize),  /// End (line,col)
+    pub line: String
 }
 
-impl TokenInfo {
-    fn get_token_type(&self) -> Result<TokenType, TryFromPrimitiveError<TokenType>> {
-        TokenType::try_from(self.token_type)
+/// We need to deal with not just a token list, but also the set of constants since the constants can change
+#[derive(Debug)]
+pub struct TokenSet {
+    /// The tokens extracted from the Python file.
+    pub tokens: Vec<TokenInfo>,
+
+    /// A mapping of token type ids to string descriptions. This is functionally an enum, but the specific
+    /// numbers seem to change, so we load it from Python to build a lookup table.
+    pub token_types: HashMap<usize, String>,
+}
+
+impl TokenSet {
+    pub fn get_token_type(&self, token_type: usize) -> Option<&String> {
+        self.token_types.get(&token_type)
     }
 }
 
 //(usize,String,(usize,usize),(usize,usize),String)
 
 /// Takes a string of bytes and returns the Python-tokenized version of it.
-pub fn tokenize(input: &str) -> PyResult<Vec<TokenInfo>> {
+pub fn tokenize(input: &str) -> PyResult<TokenSet> {
 
     let pymodule_code = include_str!("make_tokens.py");
-    println!("module: {}", pymodule_code);
 
-    Python::with_gil(|py| -> PyResult<Vec<TokenInfo>> {
+    Python::with_gil(|py| -> PyResult<TokenSet> {
         // We want to call tokenize.tokenize from Python.
         let pymodule = PyModule::from_code(py, pymodule_code, "make_tokens.py", "make_tokens")?;
         let t = pymodule.getattr("tokenize")?;
+        let h = pymodule.getattr("get_token_numbers")?;
         assert!(t.is_callable());
         let args = (input,);
         let tokens: Vec<TokenInfo> = t.call1(args)?.extract()?;
+        let token_numbers: HashMap<usize, String> = h.call0()?.extract()?;
         
-        println!("tokens: {:?}", tokens);
-
-        Ok(tokens)
+        Ok(TokenSet{
+            tokens: tokens,
+            token_types: token_numbers,
+        })
     })
 }
 
@@ -132,7 +67,7 @@ mod tests {
 def foo():
     pass
 "#).unwrap();
-        assert_eq!(result[0].get_token_type().unwrap(), TokenType::ENCODING);
+        assert_eq!(result.get_token_type(result.tokens[0].token_type), Some(&"ENCODING".to_string()));
     }
 
 
--- src/make_tokens.py
+++ src/make_tokens.py
@@ -1,7 +1,16 @@
 import tokenize as t
+import token
 from io import BytesIO
+
+def get_token_numbers():
+    lookup = {}
+    for k in token.__dict__:
+        v = token.__dict__[k]
+        if isinstance(v, int):
+            lookup[v] = k
+    return lookup
+
 
 def tokenize(input):
     val = list(t.tokenize(BytesIO(input.encode('utf-8')).readline))
-    print(val)
     return val
