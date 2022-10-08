3005617410161c25ed733651c950851c3ceff8d3
Erica Stith <rexlunae@gmail.com>
2022-10-08 12:19:14 -0700

    Return a single augmented list that includes the string descriptions.

--- src/make_tokens.py
+++ src/make_tokens.py
@@ -2,6 +2,7 @@ import tokenize as t
 import token
 from io import StringIO
 
+# Reads all the constants from the token module and makes them into a dictionary.
 def get_token_numbers():
     lookup = {}
     for k in token.__dict__:
@@ -10,7 +11,18 @@ def get_token_numbers():
             lookup[v] = k
     return lookup
 
-
+# Read the tokens from an input string and returns them as a list.
 def tokenize(input):
-    val = list(t.generate_tokens(StringIO(input).read))
+    val = list(t.generate_tokens(StringIO(input).readline))
     return val
+
+# Returns the list of tokens augmented with the text descriptions from the tokens module.
+# The numbers of these tokens can change, so the only reliable way to refer to a token type
+# is with this string, and it also makes the code more readable.
+def augment_tokens(input):
+    o = []
+    token_lookup = get_token_numbers()
+    for t in tokenize(input):
+        t.__setattr__('token_text', token_lookup[t.type])
+        o.append(t)
+    return o
